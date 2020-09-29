a3be0d33bbc6607d2c4faa6336b5ea07384c804f
devdattakhoche <dkhoche70@gmail.com>
2020-09-29 15:00:06 +0530

    Numbers

---
+++ Numbers.py
@@ -0,0 +1,18 @@
+x = 3
+print(x, type(x))
+
+#Basic Operations
+
+print(x + 1)   # Addition
+print(x - 1)   # Subtraction
+print(x * 2)   # Multiplication
+print(x ** 2)  # Exponentiation
+
+x += 1 # x = x + 1
+print(x)
+x *= 2 # x = x * 2
+print(x)
+
+y = 2.5
+print(type(y))
+print(y, y + 1, y * 2, y ** 2)
